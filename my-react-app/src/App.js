a7827cc98ef544d0b03dcf281ec9e7a196ae574a
Nataliia Boiko <nataliboiko.ua@gmail.com>
2023-11-29 12:15:50 +0000

    added component Information, with description About

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from './components/Navigation.js';
 import Banner from './components/Banner.js';
 import Panel from './components/Panel.js';
+import Information from './components/Information.js';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navigation />
       <Banner />
       <Panel />
+      <Information />
     </div>
   );
 }
---
+++ my-react-app/src/components/Information.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Information.css';
+
+function Information() {
+    return (
+        <div className="information" style={{ fontFamily: 'Open Sans, sans-serif' }}>
+            <div className="information-text">
+                <h3 className="about-title">About</h3>
+                    <p className="about-text">I am a junior web developer, inspired by the world of code and technology. My strengths include problem-solving abilities, organizational skills, and attention to detail. In my professional endeavors, I am a loyal team member for whom continuous learning is an indispensable part of success. Alongside frontend development, I am actively studying Python. Excellent communication and a commitment to improvement are my key qualities.</p>
+            </div>
+        </div>
+        
+        );
+}
+
+export default Information;
